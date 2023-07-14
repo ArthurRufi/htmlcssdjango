3cb22a829b57d418a7a4906c8c3f7213f43b67f0
ArthurRufi <temparthur87@gmail.com>
2023-07-14 16:45:35 -0300

    inicio dos trabalhos em models e sql

--- site_django/recipes/models.py
+++ site_django/recipes/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+    pass
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    desc = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparetion_time = models.IntegerField()
+    preparetion_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparetion_steps = models.TextField()
+    preparetion_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_publish = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
