7b33df4c031d41c41ca9be521bc71ad9fd83e84a
ArthurRufi <temparthur87@gmail.com>
2023-07-18 12:13:53 -0300

    Django admin init, news recipes add in db

--- site_django/recipes/models.py
+++ site_django/recipes/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
-    pass
+    def __str__(self):
+        return self.name
+
 
 
 class Recipe(models.Model):
@@ -23,3 +25,6 @@ class Recipe(models.Model):
     cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
