1e59ea7c02f54a48ef230410bd9381dbd1eea56d
ArthurRufi <temparthur87@gmail.com>
2023-07-22 17:52:53 -0300

    help

--- site_django/site_django/settings.py
+++ site_django/site_django/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(1h8u6r)83-dga+wigc+cv(3b6^8gu!(vslf5o50vd$jf568y0'
+SECRET_KEY = 'django-insecure-(1h8u6r)83-dga+wigc+cv(3b6^8gu!(vslf5o50vd$jf568y0'  # noqa: E501
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
@@ -57,9 +57,9 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             BASE_DIR / 'base_templates',
-            BASE_DIR / 'praticas'/ 'recipes',
-            BASE_DIR / 'praticas'/ 'recipes' / 'partials',
-            BASE_DIR / 'praticas'/ 'recipes' / 'pages',
+            BASE_DIR / 'praticas' / 'recipes',
+            BASE_DIR / 'praticas' / 'recipes' / 'partials',
+            BASE_DIR / 'praticas' / 'recipes' / 'pages',
             BASE_DIR / 'base_templates' / 'global',
         ],
         'APP_DIRS': True,
@@ -93,16 +93,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',  # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',  # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',  # noqa: E501
     },
 ]
 
